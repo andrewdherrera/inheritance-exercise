009b29e30d247a8738d870aeeda9b7847ad2c3f7
Andrew Herrera <thinblueline2830@gmail.com>
2019-06-17 11:47:55 -0500

    Finished HMB using grub and bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+    Bee.call(this)
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+    Grub.food = '';
+    Bee.color = 'yellow';
+
 };
 
+HoneyMakerBee.prototype.eat = function(){
+ Grub.eat();
+}
+HoneyMakerBee.prototype.makeHoney = function(){
+    this.honeyPot + this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney = function(){
+    this.honeyPot - this.honeyPot--;
+}
